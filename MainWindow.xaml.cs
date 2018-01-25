935f8ab25f868bc3cafa9baca24fb3402d02b02d
Петренков Николай <@corp.itstep>
2018-01-25 09:33:45 +0600

    Homework with site

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySite
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemClick(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate("https://github.com");
+        }
+
+        private void MenuItemClick1(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate("https://www.mail.ru");
+        }
+
+        private void MenuItemClick2(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate("https://www.youtube.ru");
+        }
+
+        private void MenuItemClick3(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
